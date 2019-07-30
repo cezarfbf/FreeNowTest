64c5f4f7af94ad573b5ea2f24332208a6428489e
Cezar Filho <cezarfbf@live.com>
2019-07-30 16:53:59 -0300

    Including Swagger UI

--- src/main/java/com/freenow/api/FreenowApiApplication.java
+++ src/main/java/com/freenow/api/FreenowApiApplication.java
@@ -2,8 +2,10 @@ package com.freenow.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class FreenowApiApplication {
 
 	public static void main(String[] args) {
